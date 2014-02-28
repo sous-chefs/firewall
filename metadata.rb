c40369867a554cce87a09fd9abdced7f6f96dba5
Sean OMeara <someara@opscode.com>
2014-02-28 10:21:13 -0500

    Version bump to 0.11.7 (for development)

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'cookbooks@opscode.com'
 license          'Apache 2.0'
 description      'Provides a set of primitives for managing firewalls and associated rules.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.11.6'
+version          '0.11.7'
 
 supports 'ubuntu'
