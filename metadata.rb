64a835029f732c431d86b4ea4ea4270e2f5c7e68
Ronald Doorn <rdoorn@schubergphilis.com>
2015-03-18 11:00:02 +0100

    undo version change, leaving that up to the cookbook owner

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'cookbooks@opscode.com'
 license 'Apache 2.0'
 description 'Provides a set of primitives for managing firewalls and associated rules.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.13.0'
+version '0.11.9'
 
 supports 'ubuntu'
 supports 'redhat'
