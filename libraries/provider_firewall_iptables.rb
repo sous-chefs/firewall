7d27d2e5a52ef6d5aa9e3d0aa2c5e1e3e9465caf
Ronald Doorn <rdoorn@schubergphilis.com>
2015-03-18 10:56:03 +0100

    start blocking for real

--- libraries/provider_firewall_iptables.rb
+++ libraries/provider_firewall_iptables.rb
@@ -30,9 +30,9 @@ class Chef
         Chef::Log.debug("#{@new_resource} already enabled.")
       else
         Chef::Log.debug("#{@new_resource} is about to be enabled")
-        shell_out!("echo iptables -P INPUT DROP")
-        shell_out!("echo iptables -P OUTPUT DROP")
-        shell_out!("echo iptables -P FORWARD DROP")
+        shell_out!("iptables -P INPUT DROP")
+        shell_out!("iptables -P OUTPUT DROP")
+        shell_out!("iptables -P FORWARD DROP")
         Chef::Log.info("#{@new_resource} enabled.")
         new_resource.updated_by_last_action(true)
       end
