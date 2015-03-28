f0300e98859bb7e84f16c771625297f2083a35e5
Martin Smith <martin@mbs3.org>
2015-03-28 13:50:45 -0500

    Small name changes for testing
    A few of the tests were failing because of naming. We should get this in Travis or CircleCI soon.

--- test/unit/spec/default_spec.rb
+++ test/unit/spec/default_spec.rb
@@ -3,8 +3,8 @@ require_relative 'spec_helper'
 describe 'firewall::default' do
   let(:chef_run) { ChefSpec::SoloRunner.converge(described_recipe) }
 
-  it 'installs ufw package' do
-    expect(chef_run).to install_package('ufw')
+  it 'enables the firewall' do
+    expect(chef_run).to enable_firewall('default')
   end
 end
 
@@ -12,6 +12,14 @@ describe 'firewall-test::default' do
   let(:chef_run) { ChefSpec::SoloRunner.converge(described_recipe) }
 
   it 'enables the firewall' do
-    expect(chef_run).to enable_firewall('ufw')
+    expect(chef_run).to enable_firewall('default')
+  end
+
+  it 'creates some rules' do
+    %w(ssh22 ssh2222 addremove duplicate0).each do |r|
+      expect(chef_run).to allow_firewall_rule(r)
+    end
+
+    expect(chef_run).to deny_firewall_rule('temp1')
   end
 end
