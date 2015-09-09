95f322be1a6065f3ec42f386b4f654adc5b1aec9
Tim Smith <tsmith84@gmail.com>
2015-09-09 11:46:29 -0700

    Opscode -> Chef, supermarket metadata, additional platforms

--- metadata.rb
+++ metadata.rb
@@ -1,13 +1,22 @@
 name 'firewall'
-maintainer 'Opscode, Inc.'
-maintainer_email 'cookbooks@opscode.com'
+maintainer 'Chef Software, Inc.'
+maintainer_email 'cookbooks@chef.io'
 license 'Apache 2.0'
 description 'Provides a set of primitives for managing firewalls and associated rules.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '2.0.2'
 
-supports 'ubuntu'
-supports 'redhat'
+supports 'amazon'
 supports 'centos'
+supports 'debian'
+supports 'fedora'
+supports 'oracle'
+supports 'redhat'
+supports 'scientific'
+supports 'ubuntu'
+
 
 depends 'chef-sugar'
+
+source_url 'https://github.com/chef-cookbooks/firewall' if respond_to?(:source_url)
+issues_url 'https://github.com/chef-cookbooks/firewall/issues' if respond_to?(:issues_url)
