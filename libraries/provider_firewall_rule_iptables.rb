7820cf6f38ac30e409a8a81a71165a5aaf897b8b
Martin Smith <martin@mbs3.org>
2015-09-23 09:33:36 -0500

    Fix chefspec and foodcritic complaints
    - Foodcritic wanted node['platform_version'] vs. :platform_version
    - Add back require_relative on chefspec

--- libraries/provider_firewall_rule_iptables.rb
+++ libraries/provider_firewall_rule_iptables.rb
@@ -20,7 +20,7 @@ class Chef
     include FirewallCookbook::Helpers::Iptables
 
     provides :firewall_rule, os: 'linux', platform_family: %w(rhel fedora) do |node|
-      node[:platform_version].to_f < 7.0
+      node['platform_version'].to_f < 7.0
     end
 
     action :create do
--- test/unit/spec/default_spec.rb
+++ test/unit/spec/default_spec.rb
@@ -1,4 +1,4 @@
-require 'spec_helper'
+require_relative 'spec_helper'
 
 describe 'firewall::default' do
   let(:chef_run) { ChefSpec::SoloRunner.new(platform: 'ubuntu', version: '14.04').converge(described_recipe) }
