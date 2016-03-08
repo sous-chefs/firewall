2008ac75274cb0a5bff47965be9aa1ed0055eb85
Martin Smith <martin@mbs3.org>
2016-03-08 10:47:01 -0300

    Release v2.5.0

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'cookbooks@chef.io'
 license 'Apache 2.0'
 description 'Provides a set of primitives for managing firewalls and associated rules.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '2.4.0'
+version '2.5.0'
 
 supports 'amazon'
 supports 'centos'
