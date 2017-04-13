0fd59a66e4101b8cd240f4047522d32bacf1b4fd
Martin Smith <martin@mbs3.org>
2017-04-13 14:54:11 -0400

    Address foodcritic complaints
    Signed-off-by: Martin Smith <martin@mbs3.org>

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name 'firewall'
 maintainer 'Chef Software, Inc.'
 maintainer_email 'cookbooks@chef.io'
-license 'Apache 2.0'
+license 'Apache-2.0'
 description 'Provides a set of primitives for managing firewalls and associated rules.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '2.5.4'
