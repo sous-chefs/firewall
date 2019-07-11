a0865230f12a68e728e01c9e619ac9bc527e2b5c
Emmanuel Iturbide <manu@ipaidthat.io>
2019-07-11 14:58:38 +0200

    Fix cookstyle errors
    Signed-off-by: Emmanuel Iturbide <manu@ipaidthat.io>

--- attributes/iptables.rb
+++ attributes/iptables.rb
@@ -1,13 +1,13 @@
-default['firewall']['iptables']['defaults'][:policy] = {
+default['firewall']['iptables']['defaults']['policy'] = {
   input: 'DROP',
   forward: 'DROP',
   output: 'ACCEPT',
 }
-default['firewall']['iptables']['defaults'][:ruleset] = {
+default['firewall']['iptables']['defaults']['ruleset'] = {
   '*filter' => 1,
-  ":INPUT #{node['firewall']['iptables']['defaults'][:policy][:input]}" => 2,
-  ":FORWARD #{node['firewall']['iptables']['defaults'][:policy][:forward]}" => 3,
-  ":OUTPUT #{node['firewall']['iptables']['defaults'][:policy][:output]}" => 4,
+  ":INPUT #{node['firewall']['iptables']['defaults']['policy']['input']}" => 2,
+  ":FORWARD #{node['firewall']['iptables']['defaults']['policy']['forward']}" => 3,
+  ":OUTPUT #{node['firewall']['iptables']['defaults']['policy']['output']}" => 4,
   'COMMIT_FILTER' => 100,
 }
 
