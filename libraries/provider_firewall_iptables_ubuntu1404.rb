67273bf0803a3a902cd85ce3cf0c66eae835f1ac
Tim Smith <tsmith84@gmail.com>
2019-10-07 16:28:25 -0700

    Resolve Cookstyle 5.8 warnings
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- libraries/provider_firewall_iptables_ubuntu1404.rb
+++ libraries/provider_firewall_iptables_ubuntu1404.rb
@@ -1,9 +1,9 @@
 #
 # Author:: Seth Chisamore (<schisamo@opscode.com>)
-# Cookbook Name:: firewall
+# Cookbook:: firewall
 # Resource:: default
 #
-# Copyright:: 2011, Opscode, Inc.
+# Copyright:: 2011-2019, Chef Software, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,6 @@ maintainer 'Chef Software, Inc.'
 maintainer_email 'cookbooks@chef.io'
 license 'Apache-2.0'
 description 'Provides a set of primitives for managing firewalls and associated rules.'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '2.7.0'
 
 supports 'centos'
@@ -15,4 +14,4 @@ depends 'chef-sugar'
 
 source_url 'https://github.com/chef-cookbooks/firewall'
 issues_url 'https://github.com/chef-cookbooks/firewall/issues'
-chef_version '>= 12.5' if respond_to?(:chef_version)
+chef_version '>= 12.5'
