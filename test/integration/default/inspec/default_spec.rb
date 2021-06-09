0bc96bd8a72b499613e0d1be138ecaafc4254033
Corey Hemminger <hemminger@hotmail.com>
2021-06-09 10:00:14 -0500

    fix for tests
    Signed-off-by: Corey Hemminger <hemminger@hotmail.com>

--- test/integration/default/inspec/default_spec.rb
+++ test/integration/default/inspec/default_spec.rb
@@ -1,16 +1,19 @@
 require_relative '../../helpers/spec_helper'
 
 describe service('firewalld') do
+  it { should be_installed }
   it { should be_enabled }
   it { should be_running }
 end if firewalld?
 
 describe service('iptables') do
+  it { should be_installed }
   it { should be_enabled }
   it { should be_running }
 end if iptables?
 
 describe service('ufw') do
+  it { should be_installed }
   it { should be_enabled }
   describe command('ufw status 2>&1') do
     its(:stdout) { should match(/Status: active/) }
--- test/integration/disable-firewall/inspec/disable-firewall_spec.rb
+++ test/integration/disable-firewall/inspec/disable-firewall_spec.rb
@@ -1,16 +1,19 @@
 require_relative '../../helpers/spec_helper'
 
 describe service('firewalld') do
+  it { should_not be_installed }
   it { should_not be_enabled }
   it { should_not be_running }
 end if firewalld?
 
 describe service('iptables') do
+  it { should_not be_installed }
   it { should_not be_enabled }
   it { should_not be_running }
 end if iptables?
 
 describe service('ufw') do
+  it { should_not be_installed }
   it { should_not be_enabled }
   it { should_not be_running }
 end if os.debian?
