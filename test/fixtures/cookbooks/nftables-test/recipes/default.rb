aebfac1b42bead9918182685259aba4d727f7d03
a7b81a9086 <40755819+a7b81a9086@users.noreply.github.com>
2022-04-19 11:44:22 +0200

    Do not specify defaults in test recipe
    Signed-off-by: Matthias Pausch <m.pausch@gsi.de>

--- test/fixtures/cookbooks/nftables-test/recipes/default.rb
+++ test/fixtures/cookbooks/nftables-test/recipes/default.rb
@@ -7,12 +7,10 @@ end
 nftables_rule 'allow loopback' do
   interface 'lo'
   protocol :none
-  command :allow
 end
 
 nftables_rule 'allow icmp' do
   protocol :icmp
-  command :allow
 end
 
 nftables_rule 'allow world to ssh' do
@@ -32,13 +30,11 @@ end
 nftables_rule 'established' do
   stateful [:related, :established]
   protocol :none # explicitly don't specify protocol
-  command :allow
 end
 
 # ipv6 needs ICMP to reliably work, so ensure it's enabled if ipv6
 nftables_rule 'ipv6_icmp' do
   protocol :'ipv6-icmp'
-  command :allow
 end
 
 nftables_rule 'ssh22' do
@@ -55,7 +51,7 @@ end
 # other rules
 nftables_rule 'temp1' do
   dport 1234
-  command :deny
+  command :drop
 end
 
 nftables_rule 'temp2' do
@@ -65,17 +61,15 @@ end
 
 nftables_rule 'addremove' do
   dport 1236
-  command :allow
 end
 
 nftables_rule 'addremove2' do
   dport 1236
-  command :deny
+  command :drop
 end
 
 nftables_rule 'protocolnum' do
   protocol 112
-  command :allow
 end
 
 nftables_rule 'prepend' do
@@ -93,23 +87,19 @@ end
 nftables_rule 'ipv6-source' do
   dport 80
   source '2001:db8::ff00:42:8329'
-  command :allow
 end
 
 nftables_rule 'range' do
   dport 1000..1100
-  command :allow
 end
 
 nftables_rule 'array' do
   dport [1234, 5000..5100, '5678']
-  command :allow
 end
 
 nftables_rule 'RPC Port Range In' do
   dport 5000..5100
   protocol :tcp
-  command :allow
   direction :in
 end
 
@@ -117,7 +107,6 @@ nftables_rule 'HTTP HTTPS' do
   dport [80, 443]
   protocol :tcp
   direction :out
-  command :allow
 end
 
 nftables_rule 'dport2433' do
@@ -126,29 +115,24 @@ nftables_rule 'dport2433' do
   source '127.0.0.0/8'
   dport 2433
   direction :in
-  command   :allow
 end
 
 nftables_rule 'esp' do
   protocol :esp
-  command :allow
 end
 
 nftables_rule 'ah' do
   protocol :ah
-  command :allow
 end
 
 nftables_rule 'esp-ipv6' do
   source '::'
   protocol :esp
-  command :allow
 end
 
 nftables_rule 'ah-ipv6' do
   source '::'
   protocol :ah
-  command :allow
 end
 
 nftables_rule 'redirect' do
